95e69eb8f7c854a7fd2cdfbeeaee4738b1162e58
KevinZhou168 <84091414+KevinZhou168@users.noreply.github.com>
2021-10-06 07:52:11 -0500

    basic functionality

--- app/src/main/java/com/example/firebase_learnauthexample/MainActivity.java
+++ app/src/main/java/com/example/firebase_learnauthexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebase_learnauthexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,11 +8,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText emailET, passwordET;
     TextView authStatusTV;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +30,25 @@ public class MainActivity extends AppCompatActivity {
         emailET = findViewById(R.id.emailEditText);
         passwordET = findViewById(R.id.passwordEditText);
         authStatusTV = findViewById(R.id.authMessageTextView);
+        mAuth = FirebaseAuth.getInstance();
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            authStatusTV.setText("onStart reloaded and " + currentUser.getEmail() + " is logged in");
+            // Take any action needed here when screen loads and a user is logged in
+        }
+        else {
+            authStatusTV.setText("onStart reloaded and user is null");
+            // Take any action needed here when screen loads and a user is NOT logged in
+        }
+    }
+
 
    /*
    This method will get all user input and then based on the button chose, perform the
@@ -54,14 +80,68 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+//    public void signUp(String email, String password) {
+//        authStatusTV.setText("Signed up");
+//    }
+//    public void signIn(String email, String password) {
+//        authStatusTV.setText("Signed in");
+//    }
+//    public void signOut(String email, String password) {
+//        authStatusTV.setText("Signed out");
+//
+//    }
+
     public void signUp(String email, String password) {
-        authStatusTV.setText("Signed up");
+
+        // If the email and password passed in are not null, then try to create a User
+        if (email != null && password != null) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.i("Denna", "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                authStatusTV.setText("Signed up " + user.getEmail() + " successfully");
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.i("Denna", "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                authStatusTV.setText("Signed up - FAILED");
+                            }
+                        }
+                    });
+        }
     }
+
     public void signIn(String email, String password) {
-        authStatusTV.setText("Signed in");
-    }
-    public void signOut(String email, String password) {
-        authStatusTV.setText("Signed out");
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.i("Denna", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            authStatusTV.setText("Signed in " + user.getEmail());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.i("Denna", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
 
     }
+
+    public void signOut(String email, String password) {
+        mAuth.signOut();
+        authStatusTV.setText("Signed out");
+    }
+
 }
